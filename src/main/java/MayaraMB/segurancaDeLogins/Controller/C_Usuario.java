d86b5b17bac42eed5ea773819568ca7cad5a6c10
Mayara Martinello Bastos <maybastos2021@gmail.com>
2025-01-14 08:53:10 -0300

    Folhas de estilo paginas html

--- src/main/java/MayaraMB/segurancaDeLogins/Controller/C_Usuario.java
+++ src/main/java/MayaraMB/segurancaDeLogins/Controller/C_Usuario.java
@@ -1,7 +1,14 @@
 package MayaraMB.segurancaDeLogins.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class C_Usuario {
+
+    @GetMapping("/cadastro")
+    public String mapCadastro(){
+        return "Cadastros/cadastro";
+    }
 }
